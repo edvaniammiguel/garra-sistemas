2ae0aa10a9229caaaa54403eaa701a9095b796cf
edvaniammiguel <edvaniammiguel@gmail.com>
2026-07-17 08:22:50 -0300

    Add files via upload

--- operacional/static/sw.js
+++ operacional/static/sw.js
@@ -14,7 +14,7 @@
  */
 
 const CACHE_NAME = 'garra-operacional-v15';
-const ASSETS_CACHE = 'garra-assets-v21';
+const ASSETS_CACHE = 'garra-assets-v22';
 const OFFLINE_PAGE = '/operacional/offline.html';
 
 // Assets que devem sempre estar em cache (shell)
