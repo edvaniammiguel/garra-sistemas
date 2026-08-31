580d417e0681d45edc3ba9d0e2e9af3d19080a21
edvaniammiguel <edvaniammiguel@gmail.com>
2026-08-31 15:03:40 -0300

    Add files via upload

--- operacional/static/sw.js
+++ operacional/static/sw.js
@@ -13,8 +13,8 @@
  * 4. Imagens/ícones: cache-first com limite de tamanho
  */
 
-const CACHE_NAME = 'garra-operacional-v17';
-const ASSETS_CACHE = 'garra-assets-v66';
+const CACHE_NAME = 'garra-operacional-v18';
+const ASSETS_CACHE = 'garra-assets-v67';
 const OFFLINE_PAGE = '/operacional/offline.html';
 
 // Assets que devem sempre estar em cache (shell)
